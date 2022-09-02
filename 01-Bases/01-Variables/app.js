23f7dec608fede06d1f844d5ffbc677a86512d38
javilavallenn <javilavallen02@gmail.com>
2022-09-01 22:56:55 -0300

    Tipos

--- 01-Bases/01-Variables/app.js
+++ 01-Bases/01-Variables/app.js
@@ -7,3 +7,8 @@ var name = javier; // Scope global
 // Nueva forma de declarar variables
 let day = 'monday'; // Scope local
 const age = 17; // Scope mixto (global y local)
+
+const fullName = 'Javier Lavallen'; // type string
+console.log(`Mi nombre es: ${fullName}`);
+
+const PI = 3.141592
