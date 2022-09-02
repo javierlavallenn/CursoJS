0b443aecd8413d6500bc131ffacb36797c629d95
javilavallenn <javilavallen02@gmail.com>
2022-09-01 23:37:25 -0300

    Condicionales

--- 01-Bases/02-Condicionales/app.js
+++ 01-Bases/02-Condicionales/app.js
@@ -1,18 +1,42 @@
-// Condicionales 
+// Condicionales
 
-let email = 'Tomas@gmail.com' ;
-
+let email = "Tomas@gmail.com";
 
 // Esto es una condicion
 
-if (email === 'Tomas@gmail.com'){
-    // Si el email es correcto  ejecuta este bloque de codigo
-    console.log(` Email: ${email}`);
-    let isCorrect = true;
-    console.log('El email es correcto :' + isCorrect); 
-    
-}else{
-    //Si el email no es correcto ejecuta este bloque de codigo
-    console.log(` El email${email} no es correcto`);
+if (email === "Tomas@gmail.com") {
+  // Si el email es correcto  ejecuta este bloque de codigo
+  console.log(` Email: ${email}`);
+  let isCorrect = true;
+  console.log("El email es correcto :" + isCorrect);
+} else {
+  //Si el email no es correcto ejecuta este bloque de codigo
+  console.log(` El email${email} no es correcto`);
+  //
 }
 
+let hunger = true;
+let thirst = true;
+
+if (hunger === true) {
+  console.log("tengo hambre");
+}
+
+// if(hunger === false)
+if (!hunger) {
+  console.log("No tengo hambre");
+}
+
+//if(hunger === true && thirst === true)
+if (hunger || thirst) {
+  console.log("tengo hambre y sed");
+}
+
+if (hunger != false) {
+  console.log("Hunger es distinto de false");
+}
+if (hunger != true) {
+  console.log("Hunger es igual a true");
+}
+
+/**************************Ejercicios***************************/
