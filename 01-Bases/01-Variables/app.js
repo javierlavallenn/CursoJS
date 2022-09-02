71923211e1b0e14554237e1fe2a22107b2535f4a
javilavallenn <javilavallen02@gmail.com>
2022-09-01 22:57:58 -0300

    Tipos

--- 01-Bases/01-Variables/app.js
+++ 01-Bases/01-Variables/app.js
@@ -11,4 +11,6 @@ const age = 17; // Scope mixto (global y local)
 const fullName = 'Javier Lavallen'; // type string
 console.log(`Mi nombre es: ${fullName}`);
 
-const PI = 3.141592
+const PI = 3.141592 // type float
+let street = 315 // type int 
+let istTrue = true // type boolean
