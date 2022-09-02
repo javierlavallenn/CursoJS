18303eaa40b6e03d58adc0627cf98a68211fc910
javilavallenn <javilavallen02@gmail.com>
2022-09-01 23:17:45 -0300

    Condicionales

---
+++ 01-Bases/02-Condicionales/app.js
@@ -0,0 +1,18 @@
+// Condicionales 
+
+let email = 'Tomas@gmail.com' ;
+
+
+// Esto es una condicion
+
+if (email === 'Tomas@gmail.com'){
+    // Si el email es correcto  ejecuta este bloque de codigo
+    console.log(` Email: ${email}`);
+    let isCorrect = true;
+    console.log('El email es correcto :' + isCorrect); 
+    
+}else{
+    //Si el email no es correcto ejecuta este bloque de codigo
+    console.log(` El email${email} no es correcto`);
+}
+
