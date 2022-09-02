583dffc586dd2f4700c6cb34cd7669eeece72f48
javilavallenn <javilavallen02@gmail.com>
2022-09-01 22:49:55 -0300

    Variables y Scope

---
+++ 01-Bases/01-Variables/app.js
@@ -0,0 +1,9 @@
+// Variables 
+
+
+// Vieja forma de declarar variables
+var name = javier; // Scope global
+
+// Nueva forma de declarar variables
+let day = 'monday'; // Scope local
+const age = 17; // Scope mixto (global y local)
